cbfcf48462984e409a3079ca9cdc0e11c400eaf6
vanillafrosty <dwu.inq@gmail.com>
2018-06-13 21:58:18 -0400

    fix bug and revise methodology in change problem

--- 3.js
+++ 3.js
@@ -1,24 +1,11 @@
-function changePossibilities(amount, arr) {
+//doesn't work for changePossibilities(6, [1,2,3])
+function changePossibilities(amount, numCoins, arr) {
   if (amount === 0) {
     return 1;
+  } else if (amount < 0) {
+    return 0;
+  } else if (numCoins <= 0 && amount > 0) {
+    return 0;
   }
-  if (arr.length === 1) {
-    if (amount % arr[0] === 0 && amount > 0) {
-      return 1;
-    } else {
-      return 0;
-    }
-  }
-  let total = 0;
-  let seen = {};
-  for (let i=0; i<arr.length; i++) {
-    if ((amount - arr[i] > 0) && (amount%arr[i] === 0)) {
-      total += 1;
-    }
-    if (seen[arr[i]] === undefined) {
-      total += changePossibilities(amount-arr[i], arr.slice(0,i).concat(arr.slice(i+1,arr.length)));
-      seen[amount-arr[i]] = true;
-    }
-  }
-  return total;
+  return changePossibilities(amount - arr[numCoins-1], numCoins, arr) + changePossibilities(amount, numCoins-1, arr);
 }
